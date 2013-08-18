3425ca8100388ac1935b657807748b2d95e33266
Lucas Starbuck <lucasstarbuck@gmail.com>
2013-08-18 08:52:28 -0700

    Removed old Rails command, changed application controller for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,4 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-	attr_accessible :email, :password, :password_confirmation, :remember_me, :name
-
 end
